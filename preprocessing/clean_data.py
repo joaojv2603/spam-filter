8efa91602621b30f7c8647a826f2941ab7d9cafb
Diogo Marciano <diogomarciano40@gmail.com>
2025-10-21 22:16:53 -0300

    sexta atualizacao

--- preprocessing/clean_data.py
+++ preprocessing/clean_data.py
@@ -1,25 +1,29 @@
 import pandas as pd
 import re
-import unicodedata
 
 def limpar_texto(texto):
     texto = str(texto).lower()
-    texto = ''.join(c for c in unicodedata.normalize('NFD', texto)
-                    if unicodedata.category(c) != 'Mn')
-    texto = re.sub(r"http\S+|www\S+|https\S+", '', texto)
-    texto = re.sub(r"[^a-zA-Z0-9\s]", '', texto)
+
+    texto = re.sub(r"[^\w\s.,!?@:/%&=-]", '', texto)
+
     texto = re.sub(r"\s+", " ", texto).strip()
+
     return texto
 
+
 def preprocessar_csv():
-    caminho_entrada = "data/dataset_spamfilter.csv"  # arquivo original
-    caminho_saida = "data/dataset_limpo.csv"        # arquivo que será salvo
+    caminho_entrada = "data/dataset_spamfilter.csv" 
+    caminho_saida = "data/dataset_limpo.csv"
 
     df = pd.read_csv(caminho_entrada)
-    df["label"] = df["label"].map({"golpe": "falsa", "veridico": "veridica"})
-    df["clean_text"] = df["message"].apply(limpar_texto)
-    df.to_csv(caminho_saida, index=False)
-    print(f"CSV limpo salvo em {caminho_saida}")
 
-# 🔹 Chamada da função
-preprocessar_csv()
+    df["label"] = df["label"].map({"golpe": "falsa", "veridico": "veridica"})
+
+    df["clean_text"] = df["message"].apply(limpar_texto)
+
+    df.to_csv(caminho_saida, index=False, encoding="utf-8")
+    print(f"? CSV limpo salvo em {caminho_saida}")
+
+
+if __name__ == "__main__":
+    preprocessar_csv()
